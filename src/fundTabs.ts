db7b54847ccb450055d32679938a7727c80bfd65
Thoiba <thoiba@MacBookPro.lan>
2025-09-16 10:43:42 +0530

    fund tabs

---
+++ src/fundTabs.ts
@@ -0,0 +1,8 @@
+export enum FundDetailsTab {
+  Invest = 'Invest',
+  Overview = 'Overview',
+  Performance = 'Performance',
+  Portfolio = 'Portfolio',
+}
+
+export type FundDetailsTabType = FundDetailsTab;
