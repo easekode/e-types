c9698dd4de67e688f8edb89d42e13daae8650bc9
angamba <angamba@refixd.com>
2025-12-29 18:40:09 +0530

    update draft sip api type

--- src/sip/index.ts
+++ src/sip/index.ts
@@ -14,7 +14,16 @@ export const sxpConfirmFormDataSchema = z.object({
   mandateId: z.string(),
 });
 
+export const updateDraftSipSchema = z.object({
+  sipId: z.string(),
+  investmentGoalId: z.string(),
+  amount: z.number().positive('Amount must be greater than 0').optional(),
+  startDate: DateObjOrString.optional(),
+  planId: z.string().optional(),
+});
+
 // Type inference from the schema
 export type sxpDraftFormData = z.infer<typeof sxpDraftFormDataSchema>;
 export type sxpConfirmFormData = z.infer<typeof sxpConfirmFormDataSchema>;
+export type UpdateDraftSip = z.infer<typeof updateDraftSipSchema>;
 export * from './sxpRequest';
